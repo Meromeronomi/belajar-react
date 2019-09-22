c303010527b56a43d998a47c3432662e204d62e7
Meromeronomi <lastgame5423@gmail.com>
2019-09-22 13:15:12 +0700

    react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {BrowserRouter, Route, Link} from "react-router-dom";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Home(){
+  return <h2>Halaman Home</h2>
+}
+function ListView(){
+  return <h2>Halaman Users</h2>
+}
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <nav>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/users">User</Link></li>
+          </nav>
+
+          <main>
+            <Route path="/" exact component={Home} />
+            <Route path="/users" exact component={ListView} />
+          </main>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
